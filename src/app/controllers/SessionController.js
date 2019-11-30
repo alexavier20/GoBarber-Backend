9252e0d98c6b0db124b20ffce8c1cd74a08e9466
alexavier20 <alex_xavier@outlook.com>
2019-11-30 16:06:20 -0300

    first commit

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,35 @@
+import jwt from 'jsonwebtoken';
+
+import User from '../models/User';
+import auth from '../../config/Auth';
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ error: 'user not found' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'password does not match' });
+    }
+
+    const { id, name } = user;
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, auth.secret, {
+        expiresIn: auth.expires,
+      }),
+    });
+  }
+}
+
+export default new SessionController();
---
+++ src/config/Auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '32df34cbc905cb89d90b31e4b1514390',
+  expires: '7d',
+};

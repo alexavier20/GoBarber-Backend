0cec761e6020736df9cf190375a056ff231f382e
alexavier20 <alex_xavier@outlook.com>
2019-12-11 16:44:38 -0300

    Notificando novos agendamentos

--- src/config/database.js
+++ src/config/database.js
@@ -2,8 +2,8 @@ module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
-  password: 'docker',
-  //password: '123456',
+  //password: 'docker',
+  password: '123456',
   database: 'gobarber',
   define: {
     timestamps: true,
